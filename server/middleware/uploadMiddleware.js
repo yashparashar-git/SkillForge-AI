804c956f36e267ea9894c0265b7b8400a23fbe2f
yashparashar-git <yashparashar7728@gmail.com>
2026-09-02 23:18:11 +0530

    Fix upload directory for deployment

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,12 +1,77 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// // Storage Configuration 
+// //save uploded file in  computer disk
+// const storage = multer.diskStorage({
+
+//     destination: (req, file, cb) => {
+//         cb(null, "uploads/");
+//     },
+
+//     filename: (req, file, cb) => {
+
+//         const uniqueName = Date.now() + path.extname(file.originalname);
+
+//         cb(null, uniqueName);
+
+//     }
+
+// });
+
+// // File Filter
+// const fileFilter = (req, file, cb) => {
+
+//     const allowedTypes = /pdf|doc|docx/;
+
+//     const extName = allowedTypes.test(
+//         path.extname(file.originalname).toLowerCase()
+//     );
+
+//     if (extName) {
+
+//         cb(null, true);
+
+//     } else {
+
+//         cb(new Error("Only PDF, DOC and DOCX files are allowed"));
+
+//     }
+
+// };
+
+// // Upload Middleware
+// const upload = multer({
+
+//     storage,
+
+//     fileFilter,
+
+//     limits:{
+
+//         fileSize:5 * 1024 * 1024
+
+//     }
+
+// });
+
+// module.exports = upload;
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
-// Storage Configuration 
-//save uploded file in  computer disk
+// Create uploads folder automatically
+const uploadDir = path.join(__dirname, "..", "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+// Storage Configuration
 const storage = multer.diskStorage({
 
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
 
     filename: (req, file, cb) => {
@@ -29,13 +94,9 @@ const fileFilter = (req, file, cb) => {
     );
 
     if (extName) {
-
         cb(null, true);
-
     } else {
-
         cb(new Error("Only PDF, DOC and DOCX files are allowed"));
-
     }
 
 };
@@ -47,10 +108,8 @@ const upload = multer({
 
     fileFilter,
 
-    limits:{
-
-        fileSize:5 * 1024 * 1024
-
+    limits: {
+        fileSize: 5 * 1024 * 1024
     }
 
 });
